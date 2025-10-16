7469ef63941abfa5083d1e2fdd646b9d7a2bbcaa
Dorde Zivanovic <djordjezivanovicva@gmail.com>
2025-10-16 13:36:53 +0100

    1. Separating everything in classes.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dataclasses import dataclass
 
+def add_and_normalize(X_in: torch.Tensor, X_out: torch.Tensor) -> torch.Tensor:
+    embedding_dim: int = int(X_in.shape[1])
+    input_mat = X_in + X_out
+    norm_add_X = torch.layer_norm(input = input_mat,
+                                        normalized_shape=(embedding_dim,))
+
+    return norm_add_X
+
 @dataclass
 class SelfAttentionModel:
     W_q: torch.Tensor
@@ -80,68 +88,83 @@ class SelfAttentionModel:
         # in the sentence.
         return Z
 
+@dataclass
+class MultiHeadSelfAttention:
+    emb_size: int
+    learning_dim: int
+    hidden_dim: int
+    num_heads: int
+    W_multi_head: torch.Tensor
+    self_attention_models: list[SelfAttentionModel]
 
-def add_and_normalize(X_in: torch.Tensor, X_out: torch.Tensor) -> torch.Tensor:
-    embedding_dim: int = int(X_in.shape[1])
-    input_mat = X_in + X_out
-    norm_add_X = torch.layer_norm(input = input_mat,
-                                        normalized_shape=(embedding_dim,))
+    def __init__(self, emb_size: int, learning_dim: int, hidden_dim: int, num_heads: int):
+        self.emb_size = emb_size
+        self.learning_dim = learning_dim
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+        self.self_attention_models = [SelfAttentionModel(emb_size=emb_size, learning_dim=learning_dim,
+                                              hidden_dim=hidden_dim, num_heads=num_heads) for _ in range(num_heads)]
 
-    return norm_add_X
+    def forward_pass(self, X_in: torch.Tensor) -> torch.Tensor:
+        heads_out = [self_attention_model.forward_pass(X_in) for self_attention_model in self.self_attention_models]
+        # X_in.num_rows, hidden_dim * num_heads
+        head = torch.concat(heads_out, dim=1)
+        print(f"{head=}")
+        # (hidden_dim, emb_size)
+        W_temp = SelfAttentionModel.fill_out_matrix(self.hidden_dim, self.emb_size, 3)
+        # (hidden_dim * num_heads, emb_size)
+        self.W_multi_head = torch.concat([W_temp] * num_heads, dim=0)
+        print(f"{head.shape=} {self.W_multi_head.shape=}")
+
+        # (X_in.num_rows, emb_size)
+        # emb_size because of add + batch normalization
+        multi_head_out = head @ self.W_multi_head
+        print(f"{multi_head_out=} {X_in=}")
+
+        return multi_head_out
+
+@dataclass
+class Encoder:
+    emb_size: int
+    num_heads: int
+    multi_head_self_attention: MultiHeadSelfAttention
+
+    def __init__(self, emb_size: int, num_heads: int):
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.multi_head_self_attention = MultiHeadSelfAttention(emb_size=emb_size,
+                                        learning_dim=emb_size + 1,
+                                        hidden_dim=emb_size + 2, num_heads=num_heads)
 
 
-def multi_head_self_attention(X_in: torch.Tensor, num_heads: int) -> torch.Tensor:
-    emb_size = X_in.shape[1]
-    learning_dim = X_in.shape[1] + 1
-    hidden_dim = X_in.shape[1]
-    self_attention_model = SelfAttentionModel(emb_size=emb_size, learning_dim=learning_dim,
-                                              hidden_dim=hidden_dim, num_heads=num_heads)
-    heads_out = [self_attention_model.forward_pass(X_in) for _ in range(num_heads)]
-    # X_in.num_rows, hidden_dim * num_heads
-    head = torch.concat(heads_out, dim=1)
-    print(f"{head=}")
-    # (hidden_dim, emb_size)
-    W_temp = SelfAttentionModel.fill_out_matrix(hidden_dim, emb_size, 3)
-    # (hidden_dim * num_heads, emb_size)
-    W_0 = torch.concat([W_temp] * num_heads, dim=0)
-    print(f"{head.shape=} {W_0.shape=}")
+    def forward_pass(self, X_in: torch.Tensor) -> torch.Tensor:
+        multi_head_out = self.multi_head_self_attention.forward_pass(X_in)
+        # (X_in.num_rows, emb_size)
+        norm_multi_head = add_and_normalize(X_in, multi_head_out)
 
-    # (X_in.num_rows, emb_size)
-    # emb_size because of add + batch normalization
-    multi_head_out = head @ W_0
-    print(f"{multi_head_out=} {X_in=}")
+        emb_size = X_in.shape[1]
+        linear_model = nn.Linear(emb_size, emb_size).double()
+        linear_layer_out = linear_model(norm_multi_head)
+        encoder_out = add_and_normalize(multi_head_out, linear_layer_out)
 
-    return multi_head_out
-
-def encoder(X_in: torch.Tensor, num_heads: int) -> torch.Tensor:
-    # TODO: text to positional embedding + embedding
-    # TODO: encode decoder attention
-
-    multi_head_out = multi_head_self_attention(X_in, num_heads)
-    # (X_in.num_rows, emb_size)
-    norm_multi_head = add_and_normalize(X_in, multi_head_out)
-
-    emb_size = X_in.shape[1]
-    linear_model = nn.Linear(emb_size, emb_size).double()
-    linear_layer_out = linear_model(norm_multi_head)
-    encoder_out = add_and_normalize(multi_head_out, linear_layer_out)
-
-    return encoder_out
+        return encoder_out
 
 
 def masked_multi_head_attention(X_in: torch.Tensor, num_heads: int):
     # TODO: masking
     # setting entries to infinity in the attention matrices so the future tokens do not affect
-    return multi_head_self_attention(X_in, num_heads)
+    return None #multi_head_self_attention(X_in, num_heads)
 
 
 def decoder(X_in: torch.Tensor, num_heads: int)-> torch.Tensor:
     masked_multi_head_out = masked_multi_head_attention(X_in, num_heads)
     norm_multi_head = add_and_normalize(X_in, masked_multi_head_out)
-    decoder_encoder_attention()
+    # decoder_encoder_attention = decoder_encoder_attention(X_in, num_heads)
+
     # K, V from encoder, Q from decoder (makes sense, we are querying)
     #  -> encoder-decoder attention, add & norm
 
+# def post_encoding
 # feedforward
 # add & norm
 
@@ -150,6 +173,7 @@ if __name__ == "__main__":
     X_in = torch.tensor([[1, 2, 3],
                   [4, 5, 6]], dtype=torch.float64)
     num_heads = 3
-    encoder_out = encoder(X_in, num_heads=num_heads)
+    encoder = Encoder(emb_size=X_in.shape[1], num_heads=num_heads)
+    encoder_out = encoder.forward_pass(X_in=X_in)
     print(f"Encoder out{encoder_out=}")
 
