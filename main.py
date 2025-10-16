efffe8bfd571e6aa0d09abd94d69b531ba4f853f
Dorde Zivanovic <djordjezivanovicva@gmail.com>
2025-10-16 17:32:02 +0100

    1. Adding support for the computation of the next token.

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from transformer import Transformer
 if __name__ == "__main__":
     # A 2×4 matrix
     X_in = torch.tensor([[1, 2, 3, 4],
-                  [4, 5, 6, 7]], dtype=torch.float64)
+                  [4, 5, 6, 7],
+                  [8, 9, 10, 11]], dtype=torch.float64)
     X_out = torch.tensor([[4, 5, 6, 7],
-                  [1, 2, 3, 8]], dtype=torch.float64)
+                  [1, 2, 3, 8],
+                  [20, 21, 22, 23]], dtype=torch.float64)
+    # TODO: encoding of words as tensors + position
     num_heads = 2
-    transformer = Transformer(X_in.shape[1], num_heads=num_heads)
-    transformer.forward_pass(X_in=X_in, X_out=X_out)
+    vocab = ["Mama", "Tata"]
+    transformer = Transformer(X_in.shape[1], num_heads=num_heads, vocab=vocab)
+    for token in transformer.forward_pass(X_in=X_in, X_out=X_out):
+        print(token)
 
--- transformer.py
+++ transformer.py
@@ -2,26 +2,44 @@ from dataclasses import dataclass
 from encoder import Encoder
 from decoder import Decoder
 import torch
+import torch.nn.functional as F
+from typing import Generator
 
 @dataclass
 class Transformer:
     emb_size: int
     num_heads: int
+    vocab: list[str]
+
     encoder: Encoder
     decoder: Decoder
 
-    def __init__(self, emb_size: int, num_heads: int):
+    def __init__(self, emb_size: int, num_heads: int, vocab: list[str]):
         self.num_heads = num_heads
         self.emb_size = emb_size
         self.encoder = Encoder(emb_size=emb_size, num_heads=num_heads)
         self.decoder = Decoder(emb_size=emb_size, num_heads=num_heads)
+        self.linear_layer = torch.nn.Linear(emb_size, len(vocab)).double()
+        self.vocab = vocab
 
 
-    def forward_pass(self, X_in: torch.Tensor, X_out: torch.Tensor)->torch.Tensor:
+    def forward_pass(self, X_in: torch.Tensor, X_out: torch.Tensor)->Generator[str, None, None]:
         encoder_output = self.encoder.forward_pass(X_in=X_in)
         print(f"{encoder_output=}")
-        decoder_out = self.decoder.forward_pass(X_in=X_out, encoder_output=encoder_output)
-        print(f"Decoder out{decoder_out=}")
+        num_tokens = X_out.shape[0]
+        for i in range(num_tokens - 1):
+            decoder_out = self.decoder.forward_pass(X_in=X_out[0:(i+2), :], encoder_output=encoder_output)
+            print(f"{decoder_out=}")
 
-        # TODO loop to feed the decoder the number of times we have the input.
-        return decoder_out
+            # Get the logits for the currently encoded sequence of words + the input query.
+            logits = self.linear_layer(decoder_out)[i+1]
+            # Check which of the words is to be the next one using probabilities with
+            # softmax
+            logits_prob = F.softmax(logits)
+            print(f"{logits_prob=}")
+            max_idx_word = torch.argmax(logits_prob)
+            # get the word from the vocabulary
+            word = self.vocab[max_idx_word]
+            yield word
+            # encode word and position
+            #X_out[i+1, :] = map_word_to_tensor(word, i+1)
